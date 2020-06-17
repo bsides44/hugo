7e14aa34f4413ed3816dade462e575343130d441
bsides44 <maddog.star@gmail.com>
2020-06-17 20:13:29 +1200

    add latlng, disable map

--- public/js/leaflet-map.js
+++ public/js/leaflet-map.js
@@ -1,19 +1,19 @@
 function initMap() {
-    var latLong = [50.084750, 8.247026]; // Wiesbaden
+  var latLong = [-41.292028, 174.778962]; // Wiesbaden
 
-    var map = L.map("map").setView(latLong, 5);
+  var map = L.map("map").setView(latLong, 5);
 
-    L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-      maxZoom: 18,
-    }).addTo(map);
+  L.tileLayer("https://b.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18,
+  }).addTo(map);
 
-    var icon = L.icon({
-      iconUrl: "images/map-marker.png",
-      iconSize:     [25, 41],
-      //iconUrl: "images/map-loc.png",
-      //iconSize:     [57, 66],
-    });
-    var marker = L.marker(latLong, { icon: icon }).addTo(map);
+  var icon = L.icon({
+    iconUrl: "images/map-marker.png",
+    iconSize: [25, 41],
+    //iconUrl: "images/map-loc.png",
+    //iconSize:     [57, 66],
+  });
+  var marker = L.marker(latLong, { icon: icon }).addTo(map);
 }
 document.addEventListener("DOMContentLoaded", initMap);
